5d61fabae8f98224da425fca6fea118b79a933f0
colbytmclain <colbytmclain@gmail.com>
2021-03-20 14:31:26 -0600

    added Player Info page

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/player-info">Player Info</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/player-info">Player Info</router-link>
     </div>
     <router-view/>
   </div>
@@ -14,8 +13,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-  margin: 0 15px;
 }
 
 #nav {
--- src/views/PlayerInfo.vue
+++ src/views/PlayerInfo.vue
@@ -1,5 +1,92 @@
 <template>
-    <div class="player-info">
-        <h1>Player Info</h1>
+    <div class = "wrapper">
+        <div class="player-info">
+            <header class = "player-header" >
+                <h1>Player Info</h1>
+            </header>
+        </div>
+        <div class = "info-flex">
+            <div class = "player-img">
+                <img src = "/Images/Players/Antetokounmpo.png">
+            </div>
+            <div class = "player-details">
+                <div class = "player-name">
+                    <p>Giannis Antetokounmpo</p>
+                </div>
+                <div class = "player-team">
+                    <p>Milwaukee Bucks</p>
+                </div>
+            </div>
+        </div>
+        <div class = "stats-flex">
+            <div class = "stats-FG">
+                <p>FG%</p>
+                <p>56.3%</p>
+            </div> 
+            <div class = "stats-AST">
+                <p>AST</p>
+                <p>6.2</p>
+            </div>
+            <div class = "stats-REB">
+                <p>REB</p>
+                <p>11.8</p>
+            </div>  
+            <div class = "stats-STL">
+                <p>STL</p>
+                <p>1.3</p>
+            </div> 
+            <div class = "stats-rating">
+                <p>RATING</p>
+                <p>98</p>
+            </div> 
+        </div>
     </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+
+
+</script>
+
+<style scoped>
+
+.player-header h1 {
+    font-size: 50px;
+}
+
+.info-flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(173, 171, 171);
+    border-radius: 10px;
+    padding: 20px;
+}
+
+.stats-flex {
+    margin-top: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+.player-name {
+    font-size: 45px;
+}
+
+.player-team {
+    font-size: 32px;
+}
+
+.stats-flex {
+    border: 2px solid black;
+}
+
+
+
+
+</style>
