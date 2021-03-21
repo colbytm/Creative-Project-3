dc8bb3c70581966c4e65adf7080eab86ed24f072
colbytmclain <colbytmclain@gmail.com>
2021-03-20 18:31:15 -0600

    updates to play page

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -23,6 +23,28 @@
                 <p>Giannis Antetokounmpo</p>
             </div>
         </div>
+        <div class = "ranking-flex">
+            <div class = "user-rank">
+                <div class = "user-rank-flex">
+                    <div class = "user-rank-text">
+                        <p>RANKING:</p>
+                    </div>
+                    <div class = "user-rank-num">
+                        <p>87</p>
+                    </div>
+                </div>
+            </div>
+            <div class = "random-rank">
+                <div class = "random-rank-flex">
+                    <div class = "random-rank-text">
+                        <p>RANKING:</p>
+                    </div>
+                    <div class = "random-rank-num">
+                        <p>84   </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="play-winner">
             <header class = "winner-header" >
                 <h1>Results</h1>
@@ -52,6 +74,20 @@
     justify-content: space-evenly;
 }
 
+.ranking-flex, .user-rank-flex, .random-rank-flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+
+.ranking-flex p {
+    font-size: 50px;
+}
+
+.user-rank-text, .random-rank-text {
+    margin-right: 20px;
+}
+
 .user-team, .random-team {
     border: 2px solid black;
     padding-left: 30px;
@@ -68,9 +104,15 @@
 }
 
 .play-winner {
-    margin-top: 50px;
+    margin-bottom: 50px;
 }
 
+.winner-header h1 {
+    font-size: 45px;
+}
 
+.play-winner p {
+    font-size: 26px;
+}
 
 </style>
